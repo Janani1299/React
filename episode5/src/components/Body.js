1f63ca14558253ba8a6df5c4775dd7c25b017927
Janani1299 <jananijayakumar367@gmail.com>
2023-09-27 15:36:23 +0530

    small changes

--- episode5/src/components/Body.js
+++ episode5/src/components/Body.js
@@ -87,17 +87,19 @@ import {useState} from "react";
 // };
 
 
-
 const Body = () => {
-    const[listOfRestaurant, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+    
     return(
         <div className="body">
             <div className="filter">
                 <button 
                     className="filter-btn"
                     onClick = {() => {
-                        const filteredList = listOfRestaurant.filter((res) => res.info.avgRating > 4 );
-                        setListOfRestaurant(listOfRestaurant);
+                       const filteredList = listOfRestaurant.filter((res) => (
+                            res.info.avgRatingString > 4
+                        ));
+                        setListOfRestaurant(filteredList);
                         console.log(filteredList);
                     }}
                     >
@@ -106,12 +108,16 @@ const Body = () => {
             </div>
             <div className="restro-container">
                 {
-                    resList.map((restaurant) =>
+                    listOfRestaurant.map((restaurant) =>
                     <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
                 }
             </div>
         </div>
     );
 };
+
+
+
+
 
 export default Body;
--- episode5/src/components/RestaurantCard.js
+++ episode5/src/components/RestaurantCard.js
@@ -3,7 +3,7 @@ import { CDN_URL } from "../utils/constant";
 const RestaurantCard = (props) => {
     const { resData } = props;
 
-    const {cloudinaryImageId,name,cuisines,costForTwo,avgRatingString,areaName} = resData?.info;
+    const {cloudinaryImageId,name,cuisines,costForTwo,avgRating,areaName} = resData?.info;
     return (
         <div className="restro-card">
             <img 
@@ -14,7 +14,7 @@ const RestaurantCard = (props) => {
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{costForTwo}</h4>
-            <h4>{avgRatingString}stars</h4>
+            <h4>{avgRating}stars</h4>
             <h4>{areaName}</h4>
         </div>
     );
